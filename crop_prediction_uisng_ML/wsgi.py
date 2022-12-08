af4e92821ec39ca8b74c57ee1c6bb7a5f1f47a57
jhashivam2200 <76090863+jhashivam0022@users.noreply.github.com>
2022-12-08 23:53:59 +0530

    vercel

--- crop_prediction_uisng_ML/wsgi.py
+++ crop_prediction_uisng_ML/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_prediction_uisng_ML.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'crop_prediction_uisng_ML.settings')
 
 application = get_wsgi_application()
+app = application
