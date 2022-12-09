de63962e88c1f07f998e5b04eb92c740c967b7dc
jhashivam2200 <76090863+jhashivam0022@users.noreply.github.com>
2022-12-09 23:23:20 +0530

    final commit

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_prediction_uisng_ML.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'crop_prediction_uisng_ML.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
